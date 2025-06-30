673aab5531e53af09af2f506195dd690013efdcf
Isutomu <isutomu@gmail.com>
2025-06-30 20:06:55 -0300

    :sparkles: feat: Add session and auth functionalities

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const logger = require("morgan");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const passport = require("passport");
 
 // Local Modules
 const routes = require("./routes");
 const { errorHandler } = require("./middlewares/errorHandler");
+const { PrismaClient } = require("./generated/client");
+
+// Constants
+const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL });
 
 // Server Initialization
 const app = express();
@@ -16,8 +23,33 @@ const PORT = process.env.PORT;
 // Middlewares
 app.use(helmet());
 app.use(logger("tiny"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // Session (?) Setup
+app.use(
+  expressSession({
+    proxy: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 1000 * 60 * 10,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+
+// Passport Authentication
+require("./config/passport");
+app.use(passport.session());
 
 // Routes
 app.use(
---
+++ src/config/passport.js
@@ -0,0 +1,46 @@
+// 3rd Party Modules
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+// Local Modules
+const { PrismaClient } = require("../generated/client");
+
+// Constants
+const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL });
+
+const authStrategy = new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: { username },
+    });
+    if (!user) {
+      return done({ status: 401, message: "Usuário não cadastrado" }, false);
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return done({ message: "Senha incorreta" }, false);
+    }
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
+passport.use(authStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
